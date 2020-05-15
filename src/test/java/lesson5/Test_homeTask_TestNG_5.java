d5a00c961a6603476171fd28d4acb4b914c36da9
dddddd <dddddd>
2020-05-15 09:33:34 +0300

    Home task: TestNg and DP + parameter

--- src/test/java/lesson5/Test_homeTask_TestNG_5.java
+++ src/test/java/lesson5/Test_homeTask_TestNG_5.java
@@ -1,15 +1,17 @@
 package test.java.lesson5;
 
 import main.java.home_task.homeTask_4.HomeTask_5;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class Test_homeTask_TestNG_5 {
 
+    @Parameters({"str"})
     @Test
-    public void getReverseString() {
-        String str = "Hello world!!!";
+    public void getReverseString(@Optional("Hello world!!!") String str) {
         String actualString = HomeTask_5.reverseString(str);
         String expectedString = new StringBuffer(str).reverse().toString();
         assertEquals(
